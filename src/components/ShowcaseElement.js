bdc85ee1e79542aa22e1cb5349245e4bc45f4b3d
celian <celian.riboulet@gmail.com>
2020-11-03 22:38:06 +0100

    Showcaseelement Eslint valid

--- src/components/ShowcaseElement.js
+++ src/components/ShowcaseElement.js
@@ -3,37 +3,35 @@ import '../stylesheets/showcaseElement.css';
 import Fade from 'react-reveal/Fade';
 
 function ShowcaseElement(props) {
+	const {
+		last,
+		screenWidth,
+		src,
+		isBtn
+	} = props;
 
+	if (last && screenWidth < 900) {
+		return null;
+	}
+	if (screenWidth < 900 && isBtn) {
+		return null;
+	}
 	return (
 		<Fade left>
-			{
-				props.last && props.screenWidth < 900
-					? <div />
-					: (
-						props.screenWidth > 900 || !props.isBtn
-							? (
-								<div className="showcaseContainer">
-									<img src={props.src} alt="gateau" className="showCaseImg" />
-									{props.isBtn
-										? (
-											<div className="showcaseBtn">
-												<h1>BRIZUR s'emporte</h1>
-												<h3>De 12H à 18H</h3>
-												<a href=" ">
-													<h2>VOIR</h2>
-												</a>
-											</div>
-										)
-										:							null
-									// <div className="overlay">
-									// 	<h4>Name</h4>
-									// </div>
-									}
-								</div>
-							)
-							:				<div />
+			<div className="showcaseContainer">
+				<img src={src} alt="gateau" className="showCaseImg" />
+				{isBtn
+					? (
+						<div className="showcaseBtn">
+							<h1>BRIZUR &lsquo; emporte</h1>
+							<h3>De 12H à 18H</h3>
+							<a href=" ">
+								<h2>VOIR</h2>
+							</a>
+						</div>
 					)
-			}
+					: null}
+			</div>
 		</Fade>
 	);
 }
