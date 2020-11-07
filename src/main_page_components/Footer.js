8328f319b7917b95de259037d0e18703dba341bb
celian <celian.riboulet@gmail.com>
2020-11-07 11:58:26 +0100

    Footer rework

--- src/main_page_components/Footer.js
+++ src/main_page_components/Footer.js
@@ -4,17 +4,23 @@ import '../stylesheets/Footer.css';
 function Footer() {
 	return (
 		<div className="Footer">
-			<div className="footerInfo">
+			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+			<div className="social">
+				<a href="https://www.instagram.com/brizur_restaurant/" className="fa fa-instagram"> </a>
+				<a href=" " className="fa fa-facebook"> </a>
+			</div>
+			<div className="legal-infos">
+				<a id="license" rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">©2020 <h5>Riboulet Célian</h5></a>
+				<a id="mentions-legales" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Mentions légales</a>
+			</div>
+			{/* <div className="footerInfo">
 				<p>Made by Riboulet Célian</p>
 				<a id="licenceImg" rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">
 					<img alt="Creative Commons Licence" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" />
 				</a>
 				<p>This work is licensed under a :
 				</p>
-				<a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">
-					Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License
-				</a>
-			</div>
+			</div> */}
 		</div>
 	);
 }
