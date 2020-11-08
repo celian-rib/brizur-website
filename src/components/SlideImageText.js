6e31b759d47fc383788badd6d52c36d7345c196c
Celian Riboulet <celian.riboulet@gmail.com>
2020-11-08 15:05:22 +0100

    SlideImageText component stylesheet updated and props

--- src/components/SlideImageText.js
+++ src/components/SlideImageText.js
@@ -1,19 +1,24 @@
 import { React, useState, useEffect } from 'react';
 import Fade from 'react-reveal/Fade';
-import assets from '../others/asset_sources';
+import '../stylesheets/slideImageText.css';
 
-function SlideImageText() {
+function SlideImageText(props) {
+
+	const { text, image } = props;
+
 	const [offset, setOffset] = useState(0);
+
 	useEffect(() => {
 		window.onscroll = () => {
 			setOffset(window.pageYOffset);
 		};
 	}, []);
+
 	return (
-		<div className="bodyInfoBar">
-			<img style={{ transform: `translateY(${offset * 0.2 - 100}px)` }} className="bodyInfoBar-image" src={assets.Plate2} alt="gateau" />
+		<div className="slideImageContainer">
+			<img style={{ transform: `translateY(${offset * 0.2 - 100}px)` }} className="slideImageContainer-image" src={image} alt="gateau" />
 			<Fade right cascade>
-				<h2>Qui ut desnderit sinNolor deserunt. Dolordolor incididunt voluptate laborum. Minim laboris deserunt commodo reprehenderit magna ea deserunt sunt ut aute pariatur ut laboris. Veniam ad ea esse mollit culpa qui reprehenderit ullamco do nostrud nisi officia esse culpa. Eiusmod aliqua reprehenderit labore esse tempor elit ad.</h2>
+				<h2>{text}</h2>
 			</Fade>
 		</div>
 	);
--- src/main_page_components/Body.js
+++ src/main_page_components/Body.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ShowCaseContainer from '../components/ShowCaseContainer';
 import SlideImageText from '../components/SlideImageText';
 import '../stylesheets/Body.css';
+import assets from '../others/asset_sources';
 
 function Body(props) {
 	const { screenWidth } = props;
 	return (
 		<div className="Body">
-			<SlideImageText />
+			<SlideImageText image={assets.Plate2} text="Info 1" />
 
 			<ShowCaseContainer screenWidth={screenWidth} />
 
-			<SlideImageText />
+			<SlideImageText image={assets.Plate3} text="Footer" />
 		</div>
 	);
 }
