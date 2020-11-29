5c309014eda58e7fd499d31b322b42232bafac96
celian <celian.riboulet@gmail.com>
2020-11-29 23:04:55 +0100

    Facebook linking

--- src/main_page_components/Footer.js
+++ src/main_page_components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
 			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 			<div className="social">
 				<a href="https://www.instagram.com/brizur_restaurant/" className="fa fa-instagram"> </a>
-				<a href=" " className="fa fa-facebook"> </a>
+				<a href="https://m.facebook.com/brizur.restaurant/" className="fa fa-facebook"> </a>
 			</div>
 			<div className="legal-infos">
 				<a id="license" rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">©2020 <h5>Riboulet Célian</h5></a>
