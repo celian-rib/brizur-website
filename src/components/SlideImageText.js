e9a96671e0675e58dc44118a8ef1443f4ad0cd61
celian <celian.riboulet@gmail.com>
2020-11-29 23:03:23 +0100

    Timetable responsive fix

--- src/components/SlideImageText.js
+++ src/components/SlideImageText.js
@@ -29,7 +29,12 @@ function SlideImageText(props) {
 							<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 							<h2>Ou nous trouver ?</h2>
 							<a href="https://www.google.com/maps/place/2+Rue+Tustal,+33000+Bordeaux/@44.8384437,-0.576585,17z/data=!3m1!4b1!4m5!3m4!1s0xd5527c5113685f7:0x2cbe8e3a4cbd0adf!8m2!3d44.8384437!4d-0.5743963" className="fa fa-street-view"> </a>
-							<h3>2 rue Tustal Bordeaux</h3>
+							<a href="https://www.google.com/maps/place/2+Rue+Tustal,+33000+Bordeaux/@44.8384437,-0.576585,17z/data=!3m1!4b1!4m5!3m4!1s0xd5527c5113685f7:0x2cbe8e3a4cbd0adf!8m2!3d44.8384437!4d-0.5743963">
+								<h3>
+									2 rue Tustal Bordeaux
+								</h3>
+							</a>
+
 						</div>
 					</div>
 				)
